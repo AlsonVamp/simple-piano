d8537857645ebbf233116abb8b7bbb41c8eb8f1a
Alexander Nazarov <alsnazarov@gmail.com>
2017-04-22 10:46:23 +0300

    Add howler library and test piano sounds

--- src/app/key.directive.ts
+++ src/app/key.directive.ts
@@ -1,14 +1,21 @@
-import { Directive, HostListener, ElementRef, Input } from '@angular/core';
+import { Directive, HostListener, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[key]'
 })
-export class KeyDirective {
+export class KeyDirective implements OnInit {
+  sound: Howl;
+  playing: boolean = false;
+  constructor(private el: ElementRef) {
 
-  constructor(private el: ElementRef) { }
+  }
   @Input() keyboardCode: number;
-  @Input() soundSource: string;
-
+  @Input() soundSource: string[];
+  ngOnInit() {
+    this.sound = new Howl({
+      src: this.soundSource
+    });
+  }
   @HostListener('mousedown') onMouseDown() {
     this.play();
   }
@@ -16,20 +23,27 @@ export class KeyDirective {
     this.stop();
   }
   @HostListener('document:keydown', ['$event']) onKeyDown(event) {
-    if (event.keyCode == this.keyboardCode) {
+    if (event.keyCode == this.keyboardCode && !this.playing) {
+      this.playing = true;
       this.play();
     }
 
   }
   @HostListener('document:keyup', ['$event']) onKeyUp(event) {
     if (event.keyCode == this.keyboardCode) {
+      this.playing = false;
       this.stop();
     }
   }
   private play() {
+    if (this.sound.playing)
+      this.sound.stop();
+    this.sound.play();
+
     console.log('play')
   }
   private stop() {
+    this.sound.stop();
     console.log('stop');
 
   }
