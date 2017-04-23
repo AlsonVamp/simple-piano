6f1d0e400d678d8d422f58463977ad39be6b7ffe
Alexander Nazarov <alsnazarov@gmail.com>
2017-04-23 09:55:32 +0300

    Add second octave samples

--- src/app/keyboard-layout/keyboard-layout.component.ts
+++ src/app/keyboard-layout/keyboard-layout.component.ts
@@ -19,19 +19,19 @@ export class KeyboardLayoutComponent implements OnInit {
     { note: "A2", src: "assets/sounds/Fsharp2v11.mp3", code: 72, type: "white", pressed: false, rate: 1.185 },
     { note: "A#2", src: "assets/sounds/Fsharp2v11.mp3", code: 85, type: "black", pressed: false, rate: 1.253 },
     { note: "B2", src: "assets/sounds/Fsharp2v11.mp3", code: 74, type: "white", pressed: false, rate: 1.329 },
-    { note: "C2", src: "assets/sounds/C2v11.mp3", code: 65, type: "white", pressed: false, rate: 1 },
-    { note: "C#2", src: "assets/sounds/C2v11.mp3", code: 87, type: "black", pressed: false, rate: 1.052 },
-    { note: "D2", src: "assets/sounds/C2v11.mp3", code: 83, type: "white", pressed: false, rate: 1.12 },
-    { note: "D#2", src: "assets/sounds/Dsharp2v11.mp3", code: 69, type: "black", pressed: false, rate: 1 },
-    { note: "E2", src: "assets/sounds/Dsharp2v11.mp3", code: 68, type: "white", pressed: false, rate: 1.052 },
-    { note: "F2", src: "assets/sounds/Dsharp2v11.mp3", code: 70, type: "white", pressed: false, rate: 1.115 },
-    { note: "F#2", src: "assets/sounds/Fsharp2v11.mp3", code: 84, type: "black", pressed: false, rate: 1 },
-    { note: "G2", src: "assets/sounds/Fsharp2v11.mp3", code: 71, type: "white", pressed: false, rate: 1.052 },
-    { note: "G#2", src: "assets/sounds/Fsharp2v11.mp3", code: 89, type: "black", pressed: false, rate: 1.115 },
-    { note: "A2", src: "assets/sounds/Fsharp2v11.mp3", code: 72, type: "white", pressed: false, rate: 1.185 },
-    { note: "A#2", src: "assets/sounds/Fsharp2v11.mp3", code: 85, type: "black", pressed: false, rate: 1.253 },
-    { note: "B2", src: "assets/sounds/Fsharp2v11.mp3", code: 74, type: "white", pressed: false, rate: 1.329 },
-    { note: "B2", src: "assets/sounds/Fsharp2v11.mp3", code: 74, type: "white", pressed: false, rate: 1.329 }
+    { note: "C3", src: "assets/sounds/C3v11.mp3", code: 75, type: "white", pressed: false, rate: 1 },
+    { note: "C#3", src: "assets/sounds/C3v11.mp3", code: 79, type: "black", pressed: false, rate: 1.052 },
+    { note: "D3", src: "assets/sounds/C3v11.mp3", code: 76, type: "white", pressed: false, rate: 1.12 },
+    { note: "D#3", src: "assets/sounds/Dsharp3v11.mp3", code: 80, type: "black", pressed: false, rate: 1 },
+    { note: "E3", src: "assets/sounds/Dsharp3v11.mp3", code: 59, type: "white", pressed: false, rate: 1.052 },
+    { note: "F3", src: "assets/sounds/Dsharp3v11.mp3", code: 222, type: "white", pressed: false, rate: 1.115 },
+    { note: "F#3", src: "assets/sounds/Fsharp3v11.mp3", code: 221, type: "black", pressed: false, rate: 1 },
+    { note: "G3", src: "assets/sounds/Fsharp3v11.mp3", code: null, type: "white", pressed: false, rate: 1.052 },
+    { note: "G#3", src: "assets/sounds/Fsharp3v11.mp3", code: null, type: "black", pressed: false, rate: 1.115 },
+    { note: "A3", src: "assets/sounds/Fsharp3v11.mp3", code: null, type: "white", pressed: false, rate: 1.185 },
+    { note: "A#3", src: "assets/sounds/Fsharp3v11.mp3", code: null, type: "black", pressed: false, rate: 1.253 },
+    { note: "B3", src: "assets/sounds/Fsharp3v11.mp3", code: null, type: "white", pressed: false, rate: 1.329 },
+    { note: "C4", src: "assets/sounds/Fsharp3v11.mp3", code: null, type: "white", pressed: false, rate: 1.415 }
   ];
 
   constructor() { }
